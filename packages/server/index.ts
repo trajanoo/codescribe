5866a60faf57c0b9f953e4282668737b6fe8a43c
trajanoo <brunotrajano.dev@gmail.com>
2026-04-30 02:12:54 -0300

    fix: change hardcoded port to dynamic port

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use('/api', generateRoutes);
 app.use('/api/payments', paymentsRouter);
 
-app.listen(3001, () => {
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
     console.log('server listening on 3001 port.');
 });
