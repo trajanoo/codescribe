08549e1c4b9e520364f1d84ac279acfc014e6584
trajanoo <brunotrajano.dev@gmail.com>
2026-05-01 15:52:32 -0300

    fix

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -15,7 +15,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('/{*path}', cors(corsOptions));
 
 app.post('/api/payments/webhook', express.raw({ type: 'application/json' }), webhookHandler);
 
